363a20df2f36d2bafca78badda2a477cb1f83e65
M0hArnous <mohamedfaarnous@gmail.com>
2024-10-31 00:48:20 +0200

    herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
-import Footer from './Components/Footer'
+import Footer from './Components/Footer';
+import Hero from './Components/Hero/Hero';
 const App = () => {
   return (
   <>
       <Navbar />
       <div className='pt-32 mx-auto max-w-7xl '>
+        <Hero />
         <Footer />
     </div>
   </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,58 +1,44 @@
 import React from "react";
+
 const Navbar = () => {
   return (
-    // <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-    //   <div className="container px-4 mx-auto relative text-sm">
-    //       <div className="flex justify-center items-center">
-    //           <div className="flex items-center flex-shrink">
-    //               <span className='text-2xl font-special '>الفجر</span>
-    //           </div>
-    //           <ul className='hidden lg:flex ml-14 space-x-12'>
-
-    //           </ul>
-    //       </div>
-    //   </div>
-    // </nav>
-
-    <header className="mx-auto max-w-7xl  fixed inset-x-0">
+    <header className="mx-auto max-w-7xl fixed inset-x-0">
       <div className="flex items-center justify-between p-5 shadow-lg border">
         <div>
           <nav>
             <ul className="flex gap-x-5">
               {DATA.map((nav) => (
-                 <a href={nav.route}>
-                 <li>
-                   {nav.name}
-                 </li>
-               </a>
+                <li key={nav.route}> {/* Use a unique key for each list item */}
+                  <a href={nav.route}>{nav.name}</a> {/* Place the <a> tag inside <li> */}
+                </li>
               ))}
             </ul>
           </nav>
         </div>
-        <div className="logo text-3xl font-bold text-blue-600">الفجر</div>
+        <div className="logo text-3xl font-bold text-[#0e7490]">ElFajr</div>
         <div>
-          <button className="py-1.5 px-5 bg-gradient-to-r from-teal-200 to-teal-500 text-white rounded-lg shadow-lg">Contact Us</button>
+          <button className="py-1.5 px-5 bg-gradient-to-r from-teal-200 to-teal-500 text-white rounded-lg shadow-lg">
+            Contact Us
+          </button>
         </div>
       </div>
     </header>
   );
 };
 
-export default Navbar;
-
-
-
 const DATA = [
   {
-      name: "Home",
-      route: "/"
+    name: "Home",
+    route: "/",
   },
   {
-      name: "About",
-      route: "/about"
+    name: "About",
+    route: "/about",
   },
   {
-      name: "Shop",
-      route: "/shop"
+    name: "Shop",
+    route: "/shop",
   },
-]
+];
+
+export default Navbar;
